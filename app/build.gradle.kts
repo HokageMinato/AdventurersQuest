763b87ed46047c395ae23dffa9581a766c057e35
Minato <dsparda996@gmail.com>
2025-05-09 00:18:56 +0530

    firebase analytics added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -8,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.kotlean"
+        applicationId = "com.textgames.adventurequest"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -70,4 +71,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.firebase.bom)
+    implementation(libs.firebase.analytics)
 }
